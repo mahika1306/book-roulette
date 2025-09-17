155f769374efdef309953753937e1bcaeaff7e23
Mahika <mahikadhar1@gmail.com>
2025-09-17 17:29:39 +0530

    main page template

--- src/app/components/main/main/main.ts
+++ src/app/components/main/main/main.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter, from, interval, map, Observable, of, take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './main.css'
 })
 export class Main {
+  newObs$=of([1,2,3,4,5,6,7]);
+  newInterval=interval(1000);
+  newTimer=timer(7000);
+  constructor() {
+    this.newObs$.pipe(map(arr=>arr.filter(value=>value%2==0))).subscribe(value=>{
+      console.log(value);
+    });
+    this.newInterval.pipe(take(7)).subscribe(value=>{
+      console.log(value);
+    });
+    this.newTimer.subscribe(value=>{
+      console.log("timer",value);
+    });
+  }   
 
 }
